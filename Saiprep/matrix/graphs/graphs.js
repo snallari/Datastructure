90df6bc91c632c7893ffc5a4698dee095faef0ce
Shruti Nallari <shruti.nallari@capgemini.com>
2024-09-16 10:09:32 -0400

    graphs

--- Saiprep/matrix/graphs/graphs.js
+++ Saiprep/matrix/graphs/graphs.js
@@ -17,19 +17,31 @@ class Graphs {
         if (this.adjancencyList[data1]) {
             this.adjancencyList[data1].push(data2)//This is directed graph
         }
+        if (this.adjancencyList[data2]) {
+            this.adjancencyList[data2].push(data1)//This is directed graph
+        }
     }
     removeEdge(data1, data2) {
-        if (this.adjancencyList[data1]) {
-           return  this.adjancencyList[data1].filter((v) => console.log("this is",v))//This is directed graph
-        }
+            this.adjancencyList[data1] = this.adjancencyList[data1].filter((v) => v != data2)//This is directed graph
+            this.adjancencyList[data2] = this.adjancencyList[data2].filter((v) => v != data1)//This is directed graph
+            console.log("removed", this.adjancencyList[data1])
+    }
+    removeVertex(v){
+        this.adjancencyList[v].forEach(element => {
+            this.removeEdge(v,element)
+        });
+        delete this.adjancencyList[v]
     }
 }
 
 
 let graph = new Graphs()
-graph.addVertex("sai")
-graph.addVertex("help")
-graph.addEdge("help", "I really need you")
-graph.addEdge("sai", "need you")
-console.log("sai", graph.removeEdge("sai", "need you"))
+graph.addVertex("dallas")
+graph.addVertex("tokyo")
+graph.addVertex("aspen")
+graph.addEdge("dallas", "tokyo")
+graph.addEdge("dallas", "aspen")
+console.log("sai", graph.removeEdge("dallas", "aspen"))
+// console.log("sai", graph.removeEdge("dallas", "tokyo"))
+//graph.removeVertex("dallas")
 console.log("graph", graph)
