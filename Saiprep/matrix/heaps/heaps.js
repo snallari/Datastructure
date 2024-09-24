8250b9d3f31a37de95404b5d6a8f46b83a72ea65
nallari <shruti.nallari@capgemini.com>
2024-09-24 10:30:30 -0400

    heaps delete

--- Saiprep/matrix/heaps/heaps.js
+++ Saiprep/matrix/heaps/heaps.js
@@ -33,9 +33,43 @@ class Heaps {
         this.values[b] = temp
         return this.values
     }
-//Only can delete highest priority element
-    delete(){
+    //Only can delete highest priority element
+    delete() {
+        let replace="", extract=""
+        replace = this.values.pop()
+        extract = this.values[0]
+        this.values[0] = replace
+        this.heapifyDown()
+        console.log("values", this.values)
+    }
 
+    heapifyDown() {
+        let idx = 0
+        const length = this.values.length
+        const element = this.values[0]
+        let lidx, ridx, l, r, swap
+        while (true) {
+            lidx = 2 * idx + 1
+            ridx = 2 * idx + 2
+            if (lidx < length) {
+                l = this.values[lidx]
+                if (l > element) {
+                    swap = lidx
+                }
+            }
+            if (ridx < length) {
+                r = this.values[ridx]
+                if ((r > element && swap==null) || (swap!==null && r>l) ) {
+                    swap = ridx
+                }
+            }
+            if(swap==null)break
+            this.values[idx]=this.values[swap]
+            this.values[swap]=element
+            idx=swap
+        }
+        console.log("values", this.values);
+        
     }
 }
 
@@ -46,5 +80,6 @@ console.log(heap.insert(25))
 console.log(heap.insert(5))
 console.log(heap.insert(40))
 console.log(heap.insert(35))
+console.log(heap.delete())
 // console.log(heap.insert(101))
 // console.log(heap.insert(21))
