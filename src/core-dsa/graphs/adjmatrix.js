692d2d15cf01c2155457716c9526cc468b7a63ac
shruti <shruti.nallari@gmail.com>
2025-11-08 08:12:53 -0500

    adj Matrix finally :)

---
+++ src/core-dsa/graphs/adjmatrix.js
@@ -0,0 +1,74 @@
+export default class GraphAdjacencyMatrix {
+
+    constructor(vertices) {
+        this.vertices = vertices
+        this.adjacencyMatrix = []
+        for (let i = 0; i < vertices; i++) {
+            this.adjacencyMatrix[i] = []
+            for (let j = 0; j < vertices; j++) {
+                this.adjacencyMatrix[i][j] = 0
+            }
+        }
+    }
+    addVertex(v) {
+        this.vertices++
+        for (let i = 0; i < this.adjacencyMatrix.length; i++) {
+            this.adjacencyMatrix[i][this.vertices - 1] = 0
+        }
+        this.adjacencyMatrix[this.vertices - 1] = []
+        for (let j = 0; j < this.vertices; j++) {
+            this.adjacencyMatrix[this.vertices - 1][j] = 0
+        }
+        console.log("matrix", this.adjacencyMatrix)
+        console.log("------------------")
+    }
+    addEdge(v1, v2) {
+        if (v1 >= this.vertices || v2 >= this.vertices || v1 < 0 || v2 < 0) {
+            console.log("invalid vertex")
+            return
+        }
+        //since its undirected
+        this.adjacencyMatrix[v1][v2] = 1
+        this.adjacencyMatrix[v2][v1] = 1
+    }
+    addEdgeDirected(v1, v2) {
+        if (v1 >= this.vertices || v2 >= this.vertices || v1 < 0 || v2 < 0) {
+            console.log("invalid vertex")
+            return
+        }
+        // since its directed
+        this.adjacencyMatrix[v1][v2] = 1
+        console.log("vertex", this.adjacencyMatrix)
+    }
+    bfs(startVertex) {
+        const visited = new Set()
+        const queue = [startVertex]
+        visited.add(startVertex)
+        while (queue.length > 0) {
+            const current = queue.shift()
+            console.log("Visiting:", current)
+            /**
+             * Index of the current row in the adjacency-matrix iteration.
+             *
+             * Used as the loop counter to iterate from 0 up to (but not including)
+             * the total number of vertices, accessing each row of the matrix.
+             *
+             * @type {number}
+             * @private
+             */
+            for(let cell=0; cell<this.vertices; cell++){
+                console.log("matrx", current, cell)
+                if(this.adjacencyMatrix[current][cell]==1 && !visited.has(cell)){
+                    visited.add(cell)
+                    queue.push(cell)
+                }
+            }
+        }
+    }
+}
+const g = new GraphAdjacencyMatrix(2)
+g.addVertex()
+g.addEdgeDirected(1, 2)
+g.addEdgeDirected(2, 2)
+g.bfs(1)
+console.log(g.adjacencyMatrix)
